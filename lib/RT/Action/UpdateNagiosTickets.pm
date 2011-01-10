12ecb2841f7aa5a004ace4784d23645ec516073a
sunnavy <sunnavy@bestpractical.com>
2011-01-10 12:54:37 +0800

    problem_type can be empty if it's a host problem, thanks to marc

--- lib/RT/Action/UpdateNagiosTickets.pm
+++ lib/RT/Action/UpdateNagiosTickets.pm
@@ -26,9 +26,10 @@ sub Commit {
     return unless $subject;
     if ( my ( $type, $category, $host, $problem_type, $problem_severity ) =
         $subject =~
-        m{(PROBLEM|RECOVERY)\s+(Service|Host) Alert: ([^/]+)/(.*)\s+is\s+(\w+)}i
+m{(PROBLEM|RECOVERY)\s+(Service|Host) Alert: ([^/]+)/?(.*)\s+is\s+(\w+)}i
       )
     {
+        $problem_type ||= '';
         $RT::Logger->info(
 "Extracted type, category, host, problem_type and problem_severity from
 subject with values $type, $category, $host, $problem_type and $problem_severity"
@@ -37,7 +38,8 @@ subject with values $type, $category, $host, $problem_type and $problem_severity
         $tickets->LimitQueue( VALUE => $new_ticket->Queue )
           unless RT->Config->Get('NagiosSearchAllQueues');
         $tickets->LimitSubject(
-            VALUE    => "$category Alert: $host/$problem_type",
+            VALUE => "$category Alert: $host"
+              . ( $problem_type ? "/$problem_type" : '' ),
             OPERATOR => 'LIKE',
         );
         $tickets->LimitStatus(
